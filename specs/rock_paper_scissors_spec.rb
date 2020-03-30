a1b8c7318ce4438fcbb9344c3dd66fbbfdf94635
David Eastcroft <davideastcroft95@gmail.com>
2020-03-30 21:10:13 +0100

    Refactors tests.

--- specs/rock_paper_scissors_spec.rb
+++ specs/rock_paper_scissors_spec.rb
@@ -13,22 +13,22 @@ class TestRockPaperScissors < Minitest::Test
   end
 
   def test_rock_scissors
-    assert_equal('rock wins', RockPaperScissors.play('rock', 'scissors'))
-    assert_equal('rock wins', RockPaperScissors.play('scissors', 'rock'))
+    assert_equal("Player 1 wins with rock!", RockPaperScissors.play('rock', 'scissors'))
+    assert_equal("Player 2 wins with rock!", RockPaperScissors.play('scissors', 'rock'))
   end
 
   def test_paper_rock
-    assert_equal('paper wins', RockPaperScissors.play('paper', 'rock'))
-    assert_equal('paper wins', RockPaperScissors.play('rock', 'paper'))
+    assert_equal("Player 1 wins with paper!", RockPaperScissors.play('paper', 'rock'))
+    assert_equal("Player 2 wins with paper!", RockPaperScissors.play('rock', 'paper'))
   end
 
   def test_scissors_paper
-    assert_equal('scissors win', RockPaperScissors.play('scissors', 'paper'))
-    assert_equal('scissors win', RockPaperScissors.play('paper', 'scissors'))
+    assert_equal("Player 1 wins with scissors!", RockPaperScissors.play('scissors', 'paper'))
+    assert_equal("Player 2 wins with scissors!", RockPaperScissors.play('paper', 'scissors'))
   end
 
   def test_nil
     assert_nil(RockPaperScissors.play('robot', 'alien'))
   end
-  
+
 end
