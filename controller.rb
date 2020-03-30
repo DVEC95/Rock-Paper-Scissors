f129708dcdf1bf46aa9b0e53dc23ed482643b043
David Eastcroft <davideastcroft95@gmail.com>
2020-03-30 20:58:38 +0100

    Adds home page.

--- controller.rb
+++ controller.rb
@@ -7,5 +7,10 @@ also_reload('./models/*')
 get '/:hand1/:hand2' do
   hand1 = params['hand1']
   hand2 = params['hand2']
-  return RockPaperScissors.play(hand1, hand2)
+  @result = RockPaperScissors.play(hand1, hand2)
+  erb(:result)
+end
+
+get '/' do
+  erb(:home)
 end
