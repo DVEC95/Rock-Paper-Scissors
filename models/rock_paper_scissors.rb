18fbb95292457274464ddbddd9f83ba7a3c3b585
David Eastcroft <davideastcroft95@gmail.com>
2020-03-30 20:08:42 +0100

    Adds RockPaperScissors class.

--- models/rock_paper_scissors.rb
+++ models/rock_paper_scissors.rb
@@ -1,11 +1,24 @@
-# class RockPaperScissors
-#
-#   def self.play(hand1, hand2)
-#     case result
-#     when hand1 = 'rock' && hand2 = 'paper'
-#       return 'paper wins'
-#     when hand1 = 'rock' && hand2 = 'scissors'
-#       return 'rock wins'
-#     when hand1 = 'rock' && hand2 = 'rock'
-#       return nil
-#     when hand1 = 'paper' &&
+class RockPaperScissors
+
+  def self.play(hand1, hand2)
+    if hand1 == hand2
+        return "It's a tie"
+    else
+      if hand1 == "rock" && hand2 == "paper"
+          return "paper wins"
+        elsif hand1 == "rock" && hand2 == "scissors"
+          return "rock wins"
+        elsif hand1 == "paper" && hand2 == "rock"
+          return "paper wins"
+        elsif hand1 == "paper" && hand2 == "scissors"
+          return "scissors win"
+        elsif hand1 == "scissors" && hand2 == "rock"
+          return "rock wins"
+        elsif hand1 == "scissors" && hand2 == "paper"
+          return "scissors win"
+        else return nil
+      end
+    end
+  end
+
+end
