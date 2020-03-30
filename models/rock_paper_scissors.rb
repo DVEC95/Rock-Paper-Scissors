137614d6d98c5530db53b68fc5f97ad3304c4e10
David Eastcroft <davideastcroft95@gmail.com>
2020-03-30 21:09:58 +0100

    Adds player number to result.

--- models/rock_paper_scissors.rb
+++ models/rock_paper_scissors.rb
@@ -4,19 +4,19 @@ class RockPaperScissors
     if hand1 == hand2
         return "It's a tie"
     else
-      if hand1 == "rock" && hand2 == "paper"
-          return "paper wins"
-        elsif hand1 == "rock" && hand2 == "scissors"
-          return "rock wins"
-        elsif hand1 == "paper" && hand2 == "rock"
-          return "paper wins"
-        elsif hand1 == "paper" && hand2 == "scissors"
-          return "scissors win"
-        elsif hand1 == "scissors" && hand2 == "rock"
-          return "rock wins"
-        elsif hand1 == "scissors" && hand2 == "paper"
-          return "scissors win"
-        else return nil
+      if hand1 == "rock" && hand2 == "scissors"
+        return "Player 1 wins with rock!"
+      elsif hand1 == "scissors" && hand2 == "paper"
+        return "Player 1 wins with scissors!"
+      elsif hand1 == "paper" && hand2 == "rock"
+        return "Player 1 wins with paper!"
+      elsif hand1 == "scissors" && hand2 == "rock"
+        return "Player 2 wins with rock!"
+      elsif hand1 == "paper" && hand2 == "scissors"
+        return "Player 2 wins with scissors!"
+      elsif hand1 == "rock" && hand2 == "paper"
+        return "Player 2 wins with paper!"
+      else return nil
       end
     end
   end
